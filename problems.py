2392c8ad916ed14a5d329fb9a66eba182ae8e3e5
kondurusubbamma-2003 <kondurumaidhili@gmail.com>
2026-06-30 16:30:06 +0530

    Add files via upload

---
+++ problems.py
@@ -0,0 +1,32 @@
+#1
+num=[10,20,30,40]
+sum=0
+for i in num:
+    sum=sum+i
+print(sum)
+
+#2
+name="maidhili"
+for i in name:
+    print(i)
+
+#3
+n=[1,2,3,4,5,6,7,8,9]
+for i in n:
+    if i%2==0:
+        print(i)
+
+4
+word=input("enter a words:").split()
+each_word=0
+for i in word:
+    print(i)
+
+#5
+
+num=[1,2,3,4,5,]
+total=0
+for i in num:
+      total=total+i
+average=total/len(num)
+print("average:",average)
